37d10ab25d3de719d67ce62985152c8ec8c761ed
ItsArt3x54 <136159213+ItsArt3x54@users.noreply.github.com>
2024-05-20 18:36:58 +0200

    added all ruby tools and revamped ItemUtil.java.

--- src/main/java/be/artex/item/ItemUtil.java
+++ src/main/java/be/artex/item/ItemUtil.java
@@ -2,91 +2,83 @@ package be.artex.item;
 
 import be.artex.UselessOres;
 import be.artex.item.itemTypes.CustomItem;
+import be.artex.item.itemTypes.tools.ToolItem;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.Item;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
 
 public class ItemUtil {
-    private static final HashMap<ItemGroups, ArrayList<net.minecraft.item.Item>> itemsInItemGroups;
+    private static final Map<ItemGroups, List<Item>> itemsInItemGroups = new EnumMap<>(ItemGroups.class);
 
-    public static net.minecraft.item.Item RUBY;
+    public static Item RUBY;
+    public static Item RUBY_SWORD;
+    public static Item RUBY_PICKAXE;
+    public static Item RUBY_AXE;
+    public static Item RUBY_SHOVEL;
+    public static Item RUBY_HOE;
 
-    // HashMap
     static {
-        itemsInItemGroups = new HashMap<>();
-
         for (ItemGroups group : ItemGroups.values()) {
             itemsInItemGroups.put(group, new ArrayList<>());
         }
     }
 
-    // when mod initialize
     public static void onModInit() {
-        // items;
         RUBY = registerItem(CustomItem.RUBY);
+        RUBY_SWORD = registerToolItem(ToolItem.RUBY_SWORD, SwordItem::new);
+        RUBY_PICKAXE = registerToolItem(ToolItem.RUBY_PICKAXE, PickaxeItem::new);
+        RUBY_AXE = registerToolItem(ToolItem.RUBY_AXE, AxeItem::new);
+        RUBY_SHOVEL = registerToolItem(ToolItem.RUBY_SHOVEL, ShovelItem::new);
+        RUBY_HOE = registerToolItem(ToolItem.RUBY_HOE, HoeItem::new);
 
-        // blocks;
-
-        // ItemGroups (must be at the end);
-        ItemGroupEvents.modifyEntriesEvent(net.minecraft.item.ItemGroups.INGREDIENTS).register(ItemUtil::addItemToIngredients);
-        ItemGroupEvents.modifyEntriesEvent(net.minecraft.item.ItemGroups.COMBAT).register(ItemUtil::addItemToCombat);
-        ItemGroupEvents.modifyEntriesEvent(net.minecraft.item.ItemGroups.TOOLS).register(ItemUtil::addItemToTools);
+        registerItemGroups();
     }
 
-    // add items in itemgroups
-    public static void addItemToIngredients(FabricItemGroupEntries entries) {
-        ArrayList<net.minecraft.item.Item> itemsInIngredients = itemsInItemGroups.get(ItemGroups.INGREDIENTS);
-
-        for (net.minecraft.item.Item item : itemsInIngredients) {
-            entries.add(item);
-        }
+    private static void registerItemGroups() {
+        ItemGroupEvents.modifyEntriesEvent(net.minecraft.item.ItemGroups.INGREDIENTS).register(entries -> addItemsToGroup(entries, ItemGroups.INGREDIENTS));
+        ItemGroupEvents.modifyEntriesEvent(net.minecraft.item.ItemGroups.COMBAT).register(entries -> addItemsToGroup(entries, ItemGroups.COMBAT));
+        ItemGroupEvents.modifyEntriesEvent(net.minecraft.item.ItemGroups.TOOLS).register(entries -> addItemsToGroup(entries, ItemGroups.TOOLS));
     }
 
-    public static void addItemToTools(FabricItemGroupEntries entries) {
-        ArrayList<net.minecraft.item.Item> itemsInIngredients = itemsInItemGroups.get(ItemGroups.TOOLS);
-
-        for (net.minecraft.item.Item item : itemsInIngredients) {
-            entries.add(item);
-        }
+    private static void addItemsToGroup(FabricItemGroupEntries entries, ItemGroups group) {
+        itemsInItemGroups.get(group).forEach(entries::add);
     }
 
-    public static void addItemToCombat(FabricItemGroupEntries entries) {
-        ArrayList<net.minecraft.item.Item> itemsInIngredients = itemsInItemGroups.get(ItemGroups.COMBAT);
-
-        for (net.minecraft.item.Item item : itemsInIngredients) {
-            entries.add(item);
-        }
-    }
-
-    // Register item ingame;
-    protected static Item registerItem(CustomItem item) {
+    public static Item registerItem(CustomItem item) {
         Item i = new Item(item.getSettings());
-
-        switch (item.getItemGroup()) {
-            case INGREDIENTS ->
-                itemsInItemGroups.get(ItemGroups.INGREDIENTS).add(i);
-
-            case TOOLS ->
-                itemsInItemGroups.get(ItemGroups.TOOLS).add(i);
-
-            case COMBAT ->
-                itemsInItemGroups.get(ItemGroups.COMBAT).add(i);
-
-            default -> {
-
-            }
-        }
+        addToCorrectItemGroup(item, i);
 
         return registerItem(item.getName(), i);
     }
 
-    protected static net.minecraft.item.Item registerItem(String name, net.minecraft.item.Item item) {
+    public static <T extends Item> T registerToolItem(ToolItem item, ToolItemFactory<T> factory) {
+        T i = factory.create(item.getToolMaterial(), item.getSettings());
+        addToCorrectItemGroup(item, i);
+        return Registry.register(Registries.ITEM, new Identifier(UselessOres.MODID, item.getName()), i);
+    }
+
+    private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(UselessOres.MODID, name), item);
+    }
+
+    private static void addToCorrectItemGroup(CustomItem item, Item i) {
+        itemsInItemGroups.get(item.getItemGroup()).add(i);
+    }
+
+    private static void addToCorrectItemGroup(ToolItem item, Item i) {
+        itemsInItemGroups.get(item.getItemGroup()).add(i);
+    }
+
+    @FunctionalInterface
+    interface ToolItemFactory<T extends Item> {
+        T create(ToolMaterial material, Item.Settings settings);
     }
 }
---
+++ src/main/java/be/artex/item/itemTypes/tools/ToolItem.java
@@ -0,0 +1,51 @@
+package be.artex.item.itemTypes.tools;
+
+import be.artex.item.ItemGroups;
+import be.artex.item.materials.ToolMaterials;
+import net.minecraft.item.*;
+
+public enum ToolItem {
+    RUBY_SWORD("ruby_sword",
+            new Item.Settings().attributeModifiers(ShovelItem.createAttributeModifiers(ToolMaterials.RUBY, 3.25f, -2.4f)),
+            ItemGroups.COMBAT, ToolMaterials.RUBY),
+    RUBY_PICKAXE("ruby_pickaxe",
+            new Item.Settings().attributeModifiers(PickaxeItem.createAttributeModifiers(ToolMaterials.RUBY, 1, -2.8f)),
+            ItemGroups.TOOLS, ToolMaterials.RUBY),
+    RUBY_AXE("ruby_axe",
+            new Item.Settings().attributeModifiers(AxeItem.createAttributeModifiers(ToolMaterials.RUBY, 6, -3.1f)),
+            ItemGroups.TOOLS, ToolMaterials.RUBY),
+    RUBY_SHOVEL("ruby_shovel",
+            new Item.Settings().attributeModifiers(ShovelItem.createAttributeModifiers(ToolMaterials.RUBY, 1.5f, -3)),
+            ItemGroups.TOOLS, ToolMaterials.RUBY),
+    RUBY_HOE("ruby_hoe",
+            new Item.Settings().attributeModifiers(HoeItem.createAttributeModifiers(ToolMaterials.RUBY, -2, -1)),
+            ItemGroups.TOOLS, ToolMaterials.RUBY),;
+
+    private final String name;
+    private final Item.Settings settings;
+    private final ItemGroups itemGroup;
+    private final ToolMaterials toolMaterial;
+
+    ToolItem(String name, Item.Settings settings, ItemGroups itemGroup, ToolMaterials toolMaterial) {
+        this.name = name;
+        this.settings = settings;
+        this.itemGroup = itemGroup;
+        this.toolMaterial = toolMaterial;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Item.Settings getSettings() {
+        return settings;
+    }
+
+    public ItemGroups getItemGroup() {
+        return itemGroup;
+    }
+
+    public ToolMaterials getToolMaterial() {
+        return toolMaterial;
+    }
+}
